5505daeceb51ccf8ce4cd70dc1ba721b970cdd23
Pedro Núñez <pedro.nz.lb@gmail.com>
2022-04-21 20:35:56 +0200

    added function to create latex documents from templates

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Bash functions:
+# Bash functions
 
-## Open references quickly
+## Open references
 Open a book, lecture notes or a paper using their biblatex alphabetic style citation. For example,
 ```
 $ b har77
@@ -10,3 +10,21 @@ would open the book published by Hartshorne in 1977. If there are several option
 $ p ser55
 ```
 for a paper published by Serre in 1955, then the output will say so and give the list of all available options.
+
+### Prerequisites:
+
+- Having all references named accordingly inside folders $HOME/books, $HOME/notes and $HOME/papers respectively.
+
+## Create LaTeX documents with GitHub repositories
+The documents are created from the templates in [this repository](https://github.com/pedro-nlb/latex-templates).
+The available templates are:
+
+- **Notes**: to take notes during a talk or lecture.
+- **Script**: to write scripts for talks, lectures, etc.
+- **Beamer**: for beamer presentations.
+- **Blurb**: for short expository notes about a specific topic. Both the name and the idea are inspired by [Keith Conrad's expository papers](https://kconrad.math.uconn.edu/blurbs/). Many thanks to him for these nice writings, I've used them many times!
+- **Solutions**: to write down solutions to exercises.
+
+### Prerequisites:
+
+- Cloning the GitHub repository [pedro-nlb/latex-templates](https://github.com/pedro-nlb/latex-templates) into a folder $HOME/git.
---
+++ latex-templates.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+new() {
+  # Create a new LaTeX document with the corresponding GitHub repository
+  # First argument is the type of template (beamer/blurb/notes/script/solutions)
+  # Second argument is the name of the new document (blank spaces will be replaced by hyphens)
+  cd "${HOME}/git"
+  if [ "${1}" = "beamer" ] || [ "${1}" = "blurb" ] || [ "${1}" = "notes" ] || [ "${1}" = "script" ] || [ "${1}" = "solutions" ]
+  then
+    mkdir "$(echo "${@}" | tr ' ' '-')"
+    cd "$(echo "${@}" | tr ' ' '-')"
+    cp "${HOME}/git/latex-templates/${1}.tex" "main.tex"
+    touch "refs.bib"
+    echo "# ${@:2}" >> "README.md"
+    echo "" >> "README.md"
+    echo "Document created from the ${1} template [here](https://github.com/pedro-nlb/latex-templates)." >> "README.md"
+    cp "${HOME}/git/latex-templates/auxiliary/gitignore" ".gitignore"
+    git init
+    git add .
+    git commit -m "First commit"
+    hub create -p
+    git push origin master
+    cd
+    read -p "Alias to edit main.tex [press enter if none]: "
+    if [ -n "${REPLY}" ]
+    then
+      echo "alias ${REPLY}=\"cd ~/git/$(echo "${@}" | tr ' ' '-'); vim main.tex\";" >> ".bash_aliases"
+    fi
+  else
+    echo "Please enter as a first argument the type of template to use (beamer/blurb/notes/script/solutions) and use the remaining arguments for the name of the document and repository (blank spaces will be replaced by hyphens)."
+  fi
+  source .bash_aliases;
+}
